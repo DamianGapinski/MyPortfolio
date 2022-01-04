882f5b2519bc35455dd66cf3bb9754087a166c3e
Damian Gapinski <dgapinski2@gmail.com>
2022-01-04 14:39:04 -0800

    Header done

--- portfolio/src/Header.js
+++ portfolio/src/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import img from './img/damianTestImg.jpg' ;
 
 const Header = () =>{
     return(
         <header>
-            
+            <div className='headerBox'><h1>O mnie</h1></div>
+            <div className='imgBox'>
+                <img src={img} alt='Main img'></img>
+            </div>
+            <div className='textBox'><p>lorem..</p></div>
         </header>
     )
 }
