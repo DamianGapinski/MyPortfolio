03f9a16dd47106c39555f8d0017b23cd0cfacb6f
Damian Gapinski <dgapinski2@gmail.com>
2022-01-04 12:47:35 -0800

    Nav done

---
+++ portfolio/src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () =>{
+  return(
+    <nav>
+      <ul>
+        <li href='#'>Moje projekty</li>
+        <li href='#'>CV</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav;
